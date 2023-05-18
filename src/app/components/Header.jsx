04b027f1adac0daa46a9f85edd95117700c66548
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-05-17 22:08:32 -0400

    made some changes to the search bar all i need it to make it lager when clicked in large screens

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,89 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
+import { MdSearch } from 'react-icons/md';
+import { FaPlusSquare, FaTimes, FaRegBell, FaRegClone } from 'react-icons/fa';
 
 function Header() {
+
+  const [isSearchVisible, setSearchVisible] = useState(false)
+
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+
+  const toggleSearch = () => {
+    setSearchVisible(!isSearchVisible);
+  };
+
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <div className="border border-red-500">
-      steve
-    </div>
+    <header className="bg-gray-100  p-8">
+    
+      <div className="flex items-center justify-between">
+
+
+        
+        <div className="flex items-center">
+
+
+
+        <div className="relative mr-4">
+
+                  <input
+                      type="text"
+                      placeholder="Search"
+                      className="bg-gray-700 hidden md:block rounded-full p-2 text-sm text-gray-300 focus:outline-none focus:bg-gray-600 w-full"
+                      />
+
+                  {isSearchVisible? (
+                    <div className="relative">
+                      <input
+                      
+                      type="text"
+                      placeholder="Search"
+                      className="bg-gray-700 md:hidden rounded-full p-2 text-sm text-gray-300 focus:outline-none focus:bg-gray-600 w-full"
+                      />
+                      <FaTimes
+                        className="absolute top-0 md:hidden right-0 mt-2 mr-2 text-gray-400 cursor-pointer"
+                        onClick={toggleSearch}
+                       />
+                    </div>
+
+                  ) : (
+                    <button
+                    className="block focus:outline-none text-white text-xl"
+                    onClick={toggleSearch}
+                    >
+                  {/* <MdSearch className="hidden md:block" onClick={toggleSearch} /> */}
+                  <FaPlusSquare className="md:hidden" onClick={toggleSearch} />
+                    </button>
+
+                  ) }
+        </div>
+
+         
+
+
+
+        </div>
+
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+    </header>
   )
 }
 
