c229283d293a802f2a6f3392d79e1c79dd85a80c
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-05-25 22:17:08 -0400

    made the side bar responsive with the icons

--- src/app/components/SideBar.jsx
+++ src/app/components/SideBar.jsx
@@ -1,31 +1,20 @@
 import React from 'react'
-import {FaBars  } from 'react-icons/fa';
+import {FaBars, FaArchive, FaShoppingCart} from 'react-icons/fa';
+import { TbHome2 } from 'react-icons/tb'; 
 
 
 function SideBar() {
   return (
-    <div className="fixed inset-y-0 left-0 lg:flex flex-col z-50 w-20 bg-gray-600 text-black items-center p-8 hidden">
+    <div className="fixed group inset-y-0 left-0 lg:flex flex-col z-50 w-16 bg-gray-600 text-black items-center p-8 hidden hover:w-64">
       <div className="mt-6">
         <FaBars className=""/>
       </div> 
-      <div className="mt-12">
-        <div className="w-2.5 h-2.5 bg-red-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-blue-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-orange-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-green-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-green-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-green-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-green-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-green-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-green-800 m-6 p-2"></div>
-      </div>
-      <div className="mt-12">
-        <div className="w-2.5 h-2.5 bg-yellow-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-purple-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-blue-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-red-800 m-6 p-2"></div>
-        <div className="w-2.5 h-2.5 bg-green-800 m-6 p-2"></div>
-      </div>
+      <ul className="mt-12 hover-trigger">
+        <li className="flex items-center w-2.5 h-2.5  m-6 p-2 ml-[2px] hover:ml-[-10px] duration-300"><a className="flex items-center"><TbHome2 /><span className="ml-8 invisible group-hover:visible">Dashboards</span></a></li>
+        <li className="flex items-center w-2.5 h-2.5  m-6 p-2 ml-[2px] hover:ml-[-10px] duration-300"><a className="flex items-center"><FaArchive /><span className="ml-8 invisible group-hover:visible">Orders</span></a></li>
+        <li className="flex items-center w-2.5 h-2.5  m-6 p-2 ml-[2px] hover:ml-[-10px] duration-300"><a className="flex items-center"><FaShoppingCart /><span className="ml-8 invisible group-hover:visible">Inventory</span></a></li>
+      </ul>
+      
     </div>
   )
 }
