b6098e81374f3fd654d310986501d803041e627d
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-06-12 17:03:13 -0400

    made the body cards responsive

--- src/app/components/PageRowContent.jsx
+++ src/app/components/PageRowContent.jsx
@@ -3,11 +3,11 @@ import StatusCard from './StatusCard'
 
 export default function PageRowContent() {
   return (
-    <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 pl-24 text-white">
-    <StatusCard classname={"border border-gray-300 w-[400px] h-40 gap-10 rounded-lg bg-purple-800"} headerContent={"task completed"} completedTasks={8} totalTasks={24} />
-    <StatusCard classname={"border border-gray-300 w-[400px] h-40 gap-10 rounded-lg  bg-blue-400"} headerContent={"task completed"} completedTasks={8} totalTasks={24} />
-    <StatusCard classname={"border border-gray-300 w-[400px] h-40 gap-10 rounded-lg  bg-red-400"} headerContent={"task completed"} completedTasks={8} totalTasks={24} />
-    <StatusCard classname={"border border-gray-300 w-[400px] h-40 gap-10 rounded-lg  bg-blue-900"} headerContent={"task completed"} completedTasks={8} totalTasks={24} />      
+    <div className="grid grid-cols-1 w-full md:grid-cols-2 md:pl-[17px] md:pr-[17px] lg:grid-cols-4 lg:pl-[68px] lg:pr-[50px] lg:pb-[0px] lg:mb-[0px] text-white">
+    <StatusCard classname={"border border-gray-300 m-2 mb-4  w-{97%} lg:w-{25%} lg:m-5 h-40 gap-10 rounded-lg bg-purple-800"} headerContent={"task completed"} completedTasks={8} totalTasks={24} />
+    <StatusCard classname={"border border-gray-300 m-2 mb-4 w-{97%} lg:w-{25%} lg:m-5 h-40 gap-10 rounded-lg  bg-blue-400"} headerContent={"task completed"} completedTasks={8} totalTasks={24} />
+    <StatusCard classname={"border border-gray-300 m-2 mb-4 w-{97%} lg:w-{25%} lg:m-5 h-40 gap-10 rounded-lg  bg-red-400"} headerContent={"task completed"} completedTasks={8} totalTasks={24} />
+    <StatusCard classname={"border border-gray-300 m-2 mb-4 w-{97%} lg:w-{25%} lg:m-5 h-40 gap-10 rounded-lg  bg-blue-900"} headerContent={"task completed"} completedTasks={8} totalTasks={24} />      
     </div>
   )
 }
--- src/app/components/PageSecondRowContent.jsx
+++ src/app/components/PageSecondRowContent.jsx
@@ -3,17 +3,17 @@ import StatusCard from './StatusCard'
 
 export default function PageSecondRowContent() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pl-14">
-        <StatusCard classname={"border border-gray-300  h-[500px] rounded-lg ml-8 mr-16 mt-9 bg-blue-700 "} headerContent={"Sales values"}/>
-        <StatusCard classname={"border border-gray-300  h-[500px] rounded-lg ml-8 mr-16 mt-9 bg-blue-700"} headerContent={"Total orders"}/>
-        <StatusCard classname={"border border-gray-300  h-[500px] rounded-lg ml-8 mr-16 mt-9 bg-blue-700"} headerContent={"Progress track"}/>
+    <div className="grid grid-cols-1 w-full sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 pl-0 md:pl-5 lg:pl-[78px] lg:pr-[37px] lg:pt-[0px] lg:mt-1">
+        <StatusCard classname={"border border-gray-300  h-[500px] rounded-lg m-2        md:ml-2 md:mr-7 md:mt-9 bg-blue-700 "} headerContent={"Sales values"}/>
+        <StatusCard classname={"border border-gray-300  h-[500px] rounded-lg m-2        md:ml-2 md:mr-7 md:mt-9 bg-blue-700"} headerContent={"Total orders"}/>
+        <StatusCard classname={"border border-gray-300  h-[500px] rounded-lg m-2        md:ml-2 md:mr-7 md:mt-9 bg-blue-700"} headerContent={"Progress track"}/>
 
-        <StatusCard classname={"border border-gray-300  h-[500px] rounded-lg ml-8 mr-16 mt-9 bg-orange-300 lg:col-span-2 "} headerContent={"Sales values"}/>
-        <StatusCard classname={"border border-gray-300  h-[500px] rounded-lg ml-8 mr-16 mt-9 bg-orange-300"} headerContent={"Total orders"}/>
+        <StatusCard classname={"border border-gray-300  h-[500px] rounded-lg m-2        md:ml-2 md:mr-7 md:mt-9 bg-orange-300 lg:col-span-2 "} headerContent={"Sales values"}/>
+        <StatusCard classname={"border border-gray-300  h-[500px] rounded-lg m-2        md:ml-2 md:mr-7 md:mt-9 bg-orange-300"} headerContent={"Total orders"}/>
 
-        <StatusCard classname={"border border-gray-300  h-[400px] rounded-lg ml-8 mr-16 mt-9 bg-purple-950"} headerContent={"Sales values"}/>
-        <StatusCard classname={"border border-gray-300  h-[400px] rounded-lg ml-8 mr-16 mt-9 bg-purple-950"} headerContent={"Total orders"}/>
-        <StatusCard classname={"border border-gray-300  h-[400px] rounded-lg ml-8 mr-16 mt-9 bg-purple-950"} headerContent={"Sales values"}/>
+        <StatusCard classname={"border border-gray-300  h-[400px] rounded-lg m-2        md:ml-2 md:mr-7 md:mt-9 bg-purple-950"} headerContent={"Sales values"}/>
+        <StatusCard classname={"border border-gray-300  h-[400px] rounded-lg m-2        md:ml-2 md:mr-7 md:mt-9 bg-purple-950"} headerContent={"Total orders"}/>
+        <StatusCard classname={"border border-gray-300  h-[400px] rounded-lg m-2        md:ml-2 md:mr-7 md:mt-9 bg-purple-950"} headerContent={"Sales values"}/>
 
     </div>
   )
