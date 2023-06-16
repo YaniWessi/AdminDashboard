e27a86660de31777ec08ac73c0822c92b1923a7e
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-06-15 20:17:56 -0400

    made some changes to the sidebar and added margin to the status cards

--- src/app/components/SideBar.jsx
+++ src/app/components/SideBar.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+"use client"
+import React,{ useState } from 'react'
 import {FaBars, FaArchive, FaShoppingCart} from 'react-icons/fa';
 import { TbHome2 } from 'react-icons/tb'; 
 
 
 function SideBar() {
+
+  const [ isExpanded, setIsExpanded ] = useState(false) 
+
+  const expand = () => setIsExpanded(true)
+
+  const hide = () => setIsExpanded(false)
+
   return (
-    <div className="fixed group inset-y-0 left-0 lg:flex flex-col z-50 w-16 bg-gray-600 text-black items-center p-8 hidden hover:w-64">
-      <div className="mt-6">
-        <FaBars className=""/>
-      </div> 
+    <div onMouseEnter={expand} onMouseLeave={hide} className="fixed group inset-y-0 lg:flex flex-col z-50 w-16 bg-gray-600 text-black items-center p-6 hidden hover:w-64 hover:items-start">
+      <FaBars/> 
       <ul className="mt-12 hover-trigger">
-        <li className="flex items-center w-2.5 h-2.5  m-6 p-2 ml-[2px] hover:ml-[-10px] duration-300"><a className="flex items-center"><TbHome2 /><span className="ml-8 invisible group-hover:visible">Dashboards</span></a></li>
-        <li className="flex items-center w-2.5 h-2.5  m-6 p-2 ml-[2px] hover:ml-[-10px] duration-300"><a className="flex items-center"><FaArchive /><span className="ml-8 invisible group-hover:visible">Orders</span></a></li>
-        <li className="flex items-center w-2.5 h-2.5  m-6 p-2 ml-[2px] hover:ml-[-10px] duration-300"><a className="flex items-center"><FaShoppingCart /><span className="ml-8 invisible group-hover:visible">Inventory</span></a></li>
+        <li className={`flex items-center ${isExpanded ? "-ml-2": ""} w-2.5 h-2.5  m-6 p-2 ml-[2px] hover:ml-[-16px] duration-300`}><a className="flex items-center"><TbHome2 /><span className="ml-8 invisible group-hover:visible">Dashboards</span></a></li>
+        <li className={`flex items-center ${isExpanded ? "-ml-2": ""} w-2.5 h-2.5  m-6 p-2 ml-[2px] hover:ml-[-16px] duration-300`}><a className="flex items-center"><FaArchive /><span className="ml-8 invisible group-hover:visible">Orders</span></a></li>
+        <li className={`flex items-center ${isExpanded ? "-ml-2": ""} w-2.5 h-2.5  m-6 p-2 ml-[2px] hover:ml-[-16px] duration-300`}><a className="flex items-center"><FaShoppingCart /><span className="ml-8 invisible group-hover:visible">Inventory</span></a></li>
       </ul>
-      
     </div>
   )
 }
--- src/app/components/StatusBar.jsx
+++ src/app/components/StatusBar.jsx
@@ -5,8 +5,8 @@ export default function StatusBar({ numTotalTasks, numCompletedTasks }) {
   const percentageCoefficient = 100;
   const barWidth = `${ numCompletedTasks / numTotalTasks * percentageCoefficient }%` 
   return (
-    <div className="bg-white h-1">
-      <div className="bg-green-600 h-1 " style={{width:barWidth}}>
+    <div className="bg-white h-1 my-4 ">
+      <div className="bg-green-600 h-1" style={{width:barWidth}}>
 
       </div>
     </div>
